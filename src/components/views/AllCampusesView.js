f4450925bc53c4cf0710dea426a5b5125b93b184
Jackie Yee <pi.jackyy@gmail.com>
2024-05-19 13:28:40 -0400

    allcampus styling changed and campus card is created using materialui

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -4,42 +4,70 @@ AllCampusesView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display all campuses.
 ================================================== */
-import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Button, Typography, Card, CardContent, CardMedia, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-const AllCampusesView = (props) => {
-  // If there is no campus, display a message.
-  if (!props.allCampuses.length) {
-    return <div>There are no campuses.</div>;
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    minHeight: '100vh',
+    paddingTop: theme.spacing(4),
+    color: '#ffffff',
+  },
+  gridContainer: {
+    width: '100%',
+    maxWidth: '1200px',
+    margin: 'auto',
+    padding: theme.spacing(2),
+  },
+  card: {
+    maxWidth: 345,
+    margin: theme.spacing(2),
+    backgroundColor: '#f5f5e9', 
+    borderRadius: '15px',
+  },
+  media: {
+    height: 140,
+  },
+}));
 
-  // If there is at least one campus, render All Campuses view 
+const CampusCard = ({ campus }) => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>All Campuses</h1>
-
-      {props.allCampuses.map((campus) => (
-        <div key={campus.id}>
+    <Grid item xs={12} sm={6} md={4}>
+      <Card className={classes.card}>
+        <CardMedia className={classes.media} image={campus.imageUrl || 'https://placekitten.com/200/140'} title={campus.name} />
+        <CardContent>
           <Link to={`/campus/${campus.id}`}>
-            <h2>{campus.name}</h2>
+            <Typography gutterBottom variant="h5" component="h2">{campus.name}</Typography>
           </Link>
-          <img src={campus.imageUrl} alt={campus.name + ' Campus'} style={{ width: "35rem", height: "auto" }}/>
-          <h4>campus id: {campus.id}</h4>
-          <p>{campus.address}</p>
-          <p>{campus.description}</p>
-          <hr/>
-        </div>
-      ))}
-      <br/>
-      <Link to={`/newcampus`}>
-        <button>Add New Campus</button>
-      </Link>
-      <br/><br/>
+          <Typography variant="body2" color="textSecondary" component="p">{campus.description}</Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+};
+
+const AllCampusesView = ({ allCampuses }) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container className={classes.gridContainer} spacing={3}>
+        {allCampuses.map(campus => (
+          <CampusCard key={campus.id} campus={campus} />
+        ))}
+      </Grid>
     </div>
   );
 };
 
-// Validate data type of the props passed to component.
 AllCampusesView.propTypes = {
   allCampuses: PropTypes.array.isRequired,
 };
