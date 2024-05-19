ea60463fc517832841a36d393c8e4c9493268e53
Jackie Yee <pi.jackyy@gmail.com>
2024-05-19 14:01:06 -0400

    changed styling of header buttons

--- src/components/containers/Header.js
+++ src/components/containers/Header.js
@@ -27,7 +27,7 @@ const useStyles = makeStyles(theme => ({
     color: '#333333'
   },
   appBar:{
-    backgroundColor: '#fcb6bb',
+    backgroundColor: '#f7cac9',
     shadows: ['none'],
   },
   greeting:{
@@ -39,7 +39,12 @@ const useStyles = makeStyles(theme => ({
   },
   links:{
     textDecoration: 'none',
-  }
+  },
+  headerButton: {
+    marginRight: '10px',
+    backgroundColor: '#e4f0d0', // Change the background color
+    color: '#333333', // Change the text color
+  },
 }));
 
 // Header component, displayed on every page
@@ -55,19 +60,19 @@ const Header = () => {
           </Typography>
 
           <Link className={classes.links} to={'/'} >
-            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+            <Button variant="contained" className={classes.headerButton}>
               Home
             </Button>
           </Link>
 
           <Link className={classes.links} to={'/campuses'} >
-            <Button variant="contained" color="primary" style={{marginRight: '10px'}}>
+          <Button variant="contained" className={classes.headerButton}>
               All Campuses
             </Button>
           </Link>
 
           <Link className={classes.links} to={'/students'} >
-            <Button variant="contained" color="primary">
+          <Button variant="contained" className={classes.headerButton}>
               All Students
             </Button>
           </Link>
